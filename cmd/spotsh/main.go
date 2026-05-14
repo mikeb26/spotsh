149f0d555e7b68ad2fa6774cf0012c58995cae3d
Mike Brown <github@torvosoft.com>
2026-05-14 17:10:53 -0400

    spotsh: split upgrade.go off from main.go

--- cmd/spotsh/main.go
+++ cmd/spotsh/main.go
@@ -1,4 +1,4 @@
-/* Copyright © 2022-2024 Mike Brown. All Rights Reserved.
+/* Copyright © 2022-2024,2026 Mike Brown. All Rights Reserved.
  *
  * See LICENSE file at the root of this package for license terms
  */
@@ -13,7 +13,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
-	"net/http"
 	"os"
 	"path/filepath"
 	"sort"
@@ -591,153 +590,6 @@ func testSshOnce(selectedInstance *iaws.LaunchEc2SpotResult) error {
 
 	conn.Close()
 	return nil
-}
-
-func upgradeMain(awsCfg aws.Config, args []string) error {
-	if versionText == DevVersionText {
-		fmt.Fprintf(os.Stderr, "Skipping spotsh upgrade on development version\n")
-		return nil
-	}
-	latestVer, err := getLatestVersion()
-	if err != nil {
-		return err
-	}
-	if latestVer == versionText {
-		fmt.Printf("spotsh %v is already the latest version\n",
-			versionText)
-		return nil
-	}
-
-	fmt.Printf("A new version of spotsh is available (%v). Upgrade? (Y/N) [Y]: ",
-		latestVer)
-	shouldUpgrade := "Y"
-	fmt.Scanf("%s", &shouldUpgrade)
-	shouldUpgrade = strings.ToUpper(strings.TrimSpace(shouldUpgrade))
-
-	if shouldUpgrade[0] != 'Y' {
-		return nil
-	}
-
-	fmt.Printf("Upgrading spotsh from %v to %v...\n", versionText,
-		latestVer)
-
-	return upgradeViaGithub(latestVer)
-}
-
-func getLatestVersion() (string, error) {
-	const LatestReleaseUrl = "https://api.github.com/repos/mikeb26/spotsh/releases/latest"
-
-	client := http.Client{
-		Timeout: time.Second * 30,
-	}
-
-	resp, err := client.Get(LatestReleaseUrl)
-	if err != nil {
-		return "", err
-	}
-
-	releaseJsonDoc, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	var releaseDoc map[string]any
-	err = json.Unmarshal(releaseJsonDoc, &releaseDoc)
-	if err != nil {
-		return "", err
-	}
-
-	latestRelease, ok := releaseDoc["tag_name"].(string)
-	if !ok {
-		return "", fmt.Errorf("Could not parse %v", LatestReleaseUrl)
-	}
-
-	return latestRelease, nil
-}
-
-func upgradeViaGithub(latestVer string) error {
-	const LatestDownloadFmt = "https://github.com/mikeb26/spotsh/releases/download/%v/spotsh"
-
-	client := http.Client{
-		Timeout: time.Second * 30,
-	}
-
-	resp, err := client.Get(fmt.Sprintf(LatestDownloadFmt, latestVer))
-	if err != nil {
-		return fmt.Errorf("Failed to download version %v: %w", versionText, err)
-
-	}
-
-	tmpFile, err := os.CreateTemp("", "spotsh-*")
-	if err != nil {
-		return fmt.Errorf("Failed to create temp file: %w", err)
-	}
-	binaryContent, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("Failed to download version %v: %w", versionText, err)
-	}
-	_, err = tmpFile.Write(binaryContent)
-	if err != nil {
-		return fmt.Errorf("Failed to download version %v: %w", versionText, err)
-	}
-	err = tmpFile.Chmod(0755)
-	if err != nil {
-		return fmt.Errorf("Failed to download version %v: %w", versionText, err)
-	}
-	err = tmpFile.Close()
-	if err != nil {
-		return fmt.Errorf("Failed to download version %v: %w", versionText, err)
-	}
-	myBinaryPath, err := os.Executable()
-	if err != nil {
-		return fmt.Errorf("Could not determine path to spotsh: %w", err)
-	}
-	myBinaryPath, err = filepath.EvalSymlinks(myBinaryPath)
-	if err != nil {
-		return fmt.Errorf("Could not determine path to spotsh: %w", err)
-	}
-
-	myBinaryPathBak := myBinaryPath + ".bak"
-	err = os.Rename(myBinaryPath, myBinaryPathBak)
-	if err != nil {
-		return fmt.Errorf("Could not replace existing %v; do you need to be root?: %w",
-			myBinaryPath, err)
-	}
-	err = os.Rename(tmpFile.Name(), myBinaryPath)
-	if errors.Is(err, syscall.EXDEV) {
-		// invalid cross device link occurs when rename() is attempted aross
-		// different filesystems; copy instead
-		err = ioutil.WriteFile(myBinaryPath, binaryContent, 0755)
-		_ = os.Remove(tmpFile.Name())
-	}
-	if err != nil {
-		err := fmt.Errorf("Could not replace existing %v; do you need to be root?: %w",
-			myBinaryPath, err)
-		_ = os.Rename(myBinaryPathBak, myBinaryPath)
-		return err
-	}
-	_ = os.Remove(myBinaryPathBak)
-
-	fmt.Printf("Upgrade %v to %v complete\n", myBinaryPath, latestVer)
-
-	return nil
-}
-
-func checkAndPrintUpgradeWarning() bool {
-	if versionText == DevVersionText {
-		return false
-	}
-	latestVer, err := getLatestVersion()
-	if err != nil {
-		return false
-	}
-	if latestVer == versionText {
-		return false
-	}
-
-	fmt.Fprintf(os.Stderr, "*WARN*: A new version of spotsh is available (%v). Please upgrade via 'spotsh upgrade'.\n\n",
-		latestVer)
-
-	return true
 }
 
 func getConfigDir() (string, error) {
